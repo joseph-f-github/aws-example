e255745338d14bbe0a7a3daa26cd8fa5546a9aee
joseph-f-github <142635446+joseph-f-github@users.noreply.github.com>
2023-08-18 19:33:02 -0400

    Delete package.py

--- package.py
+++
@@ -1,6 +0,0 @@
-def a_useful_function(parameter):
-    print(f"Hello from 'package.a_useful_function, You called this function with the parameter value: '{parameter}'")
-
-#-----------------------------------------------------------
-import boto3
-print("This is running in package.py when 'package' was imported in script.py...")
